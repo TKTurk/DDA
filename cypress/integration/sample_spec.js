fef63cfe5f3f0e058f7bb356ef7822b1c3a51003
TKTurk <TKTurk@protonmail.com>
2019-11-19 19:25:59 -0700

    configured vscode

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -1,10 +1,10 @@
-const sel = (s) => `img[alt="${s}"]`;
+const sel = s => `img[alt="${s}"]`
 
 describe('home page', () => {
-    it('loads right, rly???', () => {
-        cy.visit('http://localhost:8080')
-        cy.get(sel('about')).click()
-        cy.url().should("not.eq", "http://localhost:8080/")
-        cy.url().should("eq", "http://localhost:8080/html/about.html")
-    })
+  it('loads right, rly???', () => {
+    cy.visit('http://localhost:8080')
+    cy.get(sel('about')).click()
+    cy.url().should('not.eq', 'http://localhost:8080/')
+    cy.url().should('eq', 'http://localhost:8080/html/about.html')
+  })
 })
